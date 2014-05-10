9a12e2813d1d042963876f9c8ccdcb19a4f08124
snipem <mail@matthias-kuech.de>
2014-05-10 11:22:54 +0200

    Reformatted and better error avoidance

--- MoviepilotEntertain.user.js
+++ MoviepilotEntertain.user.js
@@ -1,26 +1,32 @@
 // ==UserScript==
 // @name           MoviepilotEntertain
-// @namespace      http://matthias-kuech.de 
+// @namespace      https://github.com/snipem/MoviepilotEntertain
 // @description    A JavaScript Userscript for searching Telekom Entertain EPG from Moviepilot
 // @include        http://www.moviepilot.de/*
 // ==/UserScript==
 
-//When showing tv schedule
-try {
-  for (var i=0; i<=document.getElementsByTagName('h3').length; i++) { var movieNumber = i; movieName = document.getElementsByTagName('h3')[movieNumber].getElementsByTagName('a')[0].text;
-  	var link = document.createElement('a');
+var url = document.URL;
+var indexOfTv = url.indexOf('moviepilot.de/tv/tv-tipps');
+var indexOfMovies = url.indexOf('moviepilot.de/movies/');
+
+if (indexOfTv > 0)
+{
+	//When showing tv schedule
+ 	for (var i=0; i<document.getElementsByTagName('h3').length; i++) 
+ 	{
+		var movieNumber = i; movieName = document.getElementsByTagName('h3')[movieNumber].getElementsByTagName('a')[0].text;
+		var link = document.createElement('a');
 		link.setAttribute('href','https://www.entertain-to-go.de/dt/pctv/main/#page=search.searchterm&searchTerm='+movieName+'&tab=tv');
 		link.innerHTML = 'Entertain';
 		document.getElementsByClassName("links")[movieNumber].appendChild(link);
 	}
 }
-catch (e)
+else if (indexOfMovies > 0)
 {
+	//When not showing indexOfTv
+	var movieName = movieName = document.getElementsByTagName('h1')[0].innerHTML.trim();
+	var link = document.createElement('a');
+	link.setAttribute('href','https://www.entertain-to-go.de/dt/pctv/main/#page=search.searchterm&searchTerm='+movieName+'&tab=tv');
+	link.innerHTML = 'Entertain';
+	document.getElementsByClassName("actions")[0].appendChild(link); 
 }
-
-//When showing single movie entry
-var movieName = movieName = document.getElementsByTagName('h1')[0].innerHTML.trim();
-var link = document.createElement('a');
-link.setAttribute('href','https://www.entertain-to-go.de/dt/pctv/main/#page=search.searchterm&searchTerm='+movieName+'&tab=tv');
-link.innerHTML = 'Entertain';
-document.getElementsByClassName("actions")[0].appendChild(link); 
